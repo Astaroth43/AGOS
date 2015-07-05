582504353886e1e39acc6741c59d021a06f84412
Astaroth43 <guti_gar@hotmail.com>
2015-07-05 17:53:31 -0500

    scene1

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class playerMovement : MonoBehaviour {
     Transform transPlayer;
     float speed;
-    bool isWalking, isGrounded, isJumping;
+    public bool isWalking, isGrounded, isJumping;
 	// Use this for initialization
 	void Start () {
         transPlayer = this.gameObject.GetComponent<Transform>();
-        speed = 1;
+        speed = 2;
         isWalking = isJumping = false;
         isGrounded = true;
 	}
@@ -17,26 +17,36 @@ public class playerMovement : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.W)){
             transPlayer.Translate(new Vector3(0,0,1) * Time.deltaTime * speed, Space.World);
-            
+            isWalking = true;
         }
         if (Input.GetKey(KeyCode.A)){
             transPlayer.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
-            
+            isWalking = true;
         }
         if (Input.GetKey(KeyCode.S)){
             transPlayer.Translate(new Vector3(0,0,-1) * Time.deltaTime * speed, Space.World);
-            
+            isWalking = true;
         }
         if (Input.GetKey(KeyCode.D)){
             transPlayer.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
-            
+            isWalking = true;
         }
         if (Input.GetKeyDown(KeyCode.Space)){
             this.GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
             isJumping = true;
         }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) && isWalking){            
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) && isWalking){
+            isWalking = false;
+            
         }
-		this.GetComponent<Animator>().SetFloat("speed", speed);            
+		if(isWalking){
+			if(this.GetComponent<Animation>().IsPlaying("Idle"))
+			   this.GetComponent<Animation>().Stop("Idle");
+			this.GetComponent<Animation> ().CrossFade ("Walk");
+		}else{
+			if(this.GetComponent<Animation>().IsPlaying("Walk"))
+				this.GetComponent<Animation>().Stop("Walk");
+			this.GetComponent<Animation> ().CrossFade ("Idle");
+		}
 	}
 }
